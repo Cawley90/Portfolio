e184ea3a61c9a4617e7b39745183971fe5eaa52a
Cawley90 <chrisjam90@gmail.com>
2014-08-26 22:24:19 -0700

    Layout changes
    Character select almost done, project details screen in the works.

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 	$('#intcorgi').fadeIn(1500);
 	$('#intcorgi').delay(1000).fadeOut(1000);
 	$('#cselect').delay(3500).slideDown("medium");
+	$('#pdetails').delay(3500).slideDown("medium");
+
 
 	//playSong();
 
